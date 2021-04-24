6606d1905e2ded8b8bbf4c5fe3ea1030f0dcb23d
Samuel Ogu <hello@samuelogu.com>
2021-04-24 02:32:24 +0100

    hello world solidity

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -2,6 +2,18 @@
 pragma solidity >= 0.5.0 < 0.7.0;
 
 contract HelloWorld {
+  bytes32 message;
+
   constructor() public {
+
   }
+
+  function HelloWorld(bytes32 myMessage) {
+    message = myMessage;
+  }
+
+  function getMessage() returns(bytes32) {
+    return message;
+  }
+
 }
